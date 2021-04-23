c93096799a339c34a355291675dfa43c9189d1c6
Shannon Bradley <mrktguru@gmail.com>
2021-04-22 20:29:57 -0500

    Added to client

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+import Auth from "./auth/Auth";
+import Login from "./auth/Login";
+import Signup from "./auth/Signup"
+
 
 function App() {
+  const [sessionToken, setSessionToken] = useState("");
+
+  const [userId, setUserId] = useState("");
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
+    }
+  }, []);
+
+  const updateToken = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setSessionToken(newToken);
+  };
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken("");
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("username")) {
+      setUsername(localStorage.getItem("username"));
+    }
+  }, []);
+
+  const getUsername = (username) => {
+    localStorage.setItem("username", username);
+    setUsername(username);
+  };
+
+  /// trying to pass id
+
+  useEffect(() => {
+    if (localStorage.getItem("userId")) {
+      setUserId(localStorage.getItem("userId"));
+    }
+  }, []);
+
+  const setId = (id) => {
+    localStorage.setItem("userId", id);
+    setUserId(id);
+  };
+
+  const protectedViews = () => {
+    return sessionToken === localStorage.getItem("token") ? (
+      <MainPage token={sessionToken} userId={userId} username={username} />
+    ) : (
+      <Auth updateToken={updateToken} setId={setId} getUsername={getUsername} />
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Router>
+        <Sitebar clearToken={clearToken} token={sessionToken} />
+      </Router>
+      {protectedViews()}
+      <Footer />
     </div>
   );
 }
-
 export default App;
